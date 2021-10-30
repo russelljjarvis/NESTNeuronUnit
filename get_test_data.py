10a6808d3efa29e6c2272e9dea5b1719fa16e8de
Russell Jarvis <russelljarvis@protonmail.com>
2021-10-30 17:53:19 +1100

    Update get_test_data.py

--- get_test_data.py
+++ get_test_data.py
@@ -20,12 +20,10 @@ fitnesses,scores,obs_preds,opt,target,hall_of_fame,cell_evaluator = optimize_job
                                              efel_filter_iterable=efel_filter_iterable)
  
 
-ith open('325479788later_allen_NU_tests.p', "rb") as f:
-    suite = pickle.load(f)
-    
+"""    
 plt.plot(suite.traces["vm_soma"].times,suite.traces["vm_soma"])
 plt.xlabel(pq.s)
 plt.ylabel(suite.traces["vm_soma"].dimensionality)
 plt.title("$V_{M}$ Allen Specimen id 325479788, sweep number 64")
 plt.show()
-
+"""
