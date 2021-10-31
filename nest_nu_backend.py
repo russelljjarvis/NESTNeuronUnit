493b1b1d2cad261a147aac3304be84dcf695a5be
Russell Jarvis <russelljarvis@protonmail.com>
2021-10-31 13:11:40 +1100

    before pull remote

--- nest_nu_backend.py
+++ nest_nu_backend.py
@@ -64,21 +64,3 @@ class NESTADEXP(RunnableModel):
         results["t"] = self.vM.times
         results["run_number"] = results.get("run_number", 0) + 1
         return results
-
-
-"""
-model_params = {"V_peak": 20.0, "E_L": -60.0, "a": 80.0, "b": 80.5, "tau_w": 720.0}
-model = NESTADEXP(nest=nest, attrs=model_params)
-inject_param = {
-    "start": 0 * qt.ms,
-    "stop": 1000 * qt.ms,
-    "delay": 100 * qt.ms,
-    "amplitude": 900 * qt.pA,
-    "duration": 1200 * qt.ms,
-}
-model.inject_square_current(inject_param)
-vm = model.get_membrane_potential()
-plt.plot(vm.times, vm)
-plt.show()
-"""
-# print(model.get_spike_count())
--- test_interface.py
+++ test_interface.py
@@ -1,15 +1,9 @@
 from nest_nu_backend import *
-
-model_params = {"V_peak": 20.0, "E_L": -60.0, "a": 80.0, "b": 80.5, "tau_w": 720.0}
-model = NESTADEXP(nest=nest, attrs=model_params)
-inject_param = {
-    "start": 0 * qt.ms,
-    "stop": 1000 * qt.ms,
-    "delay": 100 * qt.ms,
-    "amplitude": 900 * qt.pA,
-    "duration": 1200 * qt.ms,
-}
+model_params={'V_peak':20., 'E_L':-60.0, 'a':80.0, 'b':80.5, 'tau_w':720.0}
+model = NESTADEXP(nest=nest,attrs=model_params)
+inject_param={'start':0*qt.ms,'stop':1000*qt.ms,'delay':100*qt.ms,'amplitude':900*qt.pA,'duration':1200*qt.ms}
 model.inject_square_current(inject_param)
+print(dir(model))
 vm = model.get_membrane_potential()
-plt.plot(vm.times, vm)
+plt.plot(vm.times,vm)
 plt.show()
